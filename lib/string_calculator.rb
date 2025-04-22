ce3f736b32ab1ab6fb2b79d5cc26aa715994a873
sauravgr458 <sauravgr854@gmail.com>
2025-04-22 15:14:01 +0530

    fix: Rubocop offenses

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 # lib/string_calculator.rb
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    if numbers.start_with?("//")
+    if numbers.start_with?('//')
       delimiter, numbers = numbers[2..].split("\n", 2)
     else
       delimiter = /,|\n/
     end
 
     num_list = numbers.split(delimiter).map(&:to_i)
-    negatives = num_list.select { |num| num < 0 }
+    negatives = num_list.select(&:negative?)
 
     raise "Negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
 
