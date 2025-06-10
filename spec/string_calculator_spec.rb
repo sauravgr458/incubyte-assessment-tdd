c12d02e5df5b15dc079646054619ac8107740eb6
sauravgr458 <sauravgr854@gmail.com>
2025-06-10 12:00:00 +0530

    test: Add RSpec case for newline delimiter support

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -18,4 +18,8 @@ RSpec.describe StringCalculator do
   it "returns sum of multiple numbers" do
     expect(StringCalculator.add("1,2,3,4")).to eq(10)
   end
+
+  it "handles newlines as delimiters" do
+    expect(StringCalculator.add("1\n2,3")).to eq(6)
+  end
 end
